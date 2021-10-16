d60364a4329afcddc2168d7abcf7d1befa5b36bd
melissa-24 <melissalongenberger24@gmail.com>
2021-10-15 20:30:54 -0400

    fixed admin.py

--- officalZooApp/admin.py
+++ officalZooApp/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Zoo
+from .models import *
 
 admin.site.register(Zoo)
+admin.site.register(Shop)
+admin.site.register(Animal)
+admin.site.register(Employee)
+admin.site.register(Show)
